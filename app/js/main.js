307f8839797802127b3336b5f9af3d8f81f46105
The-Banshee <vakarianamtb@gmail.com>
2023-08-24 21:54:52 +0300

    added section about-vr

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const clientHeight = document.documentElement.clientHeight;
+
     function addRelevantPaddingToSectionHero() {
         const sectionHero = document.querySelector('.hero');
-        const clientHeight = document.documentElement.clientHeight;
-
         const sectionHeroPaddingToAdd = clientHeight - 742;
         sectionHero.style.paddingBottom = `${sectionHeroPaddingToAdd}px`;
     }
     addRelevantPaddingToSectionHero();
 
+    function addRelevantPaddingToSectionAboutVR(){
+        const sectionAboutVR = document.querySelector('.about-vr');
+        const sectionAboutVRHeight = sectionAboutVR.offsetHeight;
+        const sectionAboutVRPaddingToAdd = (clientHeight - sectionAboutVRHeight) / 2;
+        sectionAboutVR.style.padding = `${sectionAboutVRPaddingToAdd}px 0`;
+    }
+    addRelevantPaddingToSectionAboutVR();
+
     window.addEventListener('resize', () => {
         addRelevantPaddingToSectionHero();
+        addRelevantPaddingToSectionAboutVR();
     });
 
 });
